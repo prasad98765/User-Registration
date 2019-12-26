53b92d4ee541220b7a64e6596de8455a297b9e0d
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-26 09:26:25 +0530

    MODIFIED : Declare 'UserRegistration' Class Object As Static

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -3,87 +3,76 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
+
+    public static UserRegistration checkUserRegistration  = new UserRegistration();
     @Test
     public void whenGivenName_CheckFirstCharCapital_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
         Assert.assertTrue(checkUserRegistration.checkFirstName("Prasad"));
     }
 
     @Test
     public void whenGivenName_CheckFirstCharNotCapital_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration = new UserRegistration();
         Assert.assertFalse(checkUserRegistration.checkFirstName("prasad"));
     }
 
     @Test
     public void whenGivenLastName_CheckFirstCharCapital_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
         Assert.assertTrue(checkUserRegistration.checkLastName("Chaudhari"));
     }
 
     @Test
     public void whenGivenLastName_CheckFirstCharNotCapital_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration = new UserRegistration();
         Assert.assertFalse(checkUserRegistration.checkLastName("chaudhari"));
     }
 
     @Test
      public void whenGivenEmail_CheckEmailValid_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
         Assert.assertTrue(checkUserRegistration.checkEmail("abc.xyz@bl.co.in"));
     }
 
     @Test
     public void whenGivenEmail_CheckEmailInvalid_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
         Assert.assertFalse(checkUserRegistration.checkEmail("abc123@.com"));
     }
 
     @Test
     public void whenGivenMobileNumber_CheckMobileNumberValid_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
         Assert.assertTrue(checkUserRegistration.checkMobileNumber("+91 7798917132"));
     }
 
     @Test
     public void whenGivenMobileNumber_CheckMobileNumberNotValid_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
         Assert.assertFalse(checkUserRegistration.checkMobileNumber("+9 7798917132"));
     }
 
     @Test
     public void whenGivenPassword_CheckEightCharacters_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
-        Assert.assertTrue(checkUserRegistration.checkPassword("asdF1234"));
+        Assert.assertTrue(checkUserRegistration.checkPassword("asdF$123"));
     }
 
     @Test
     public void whenGivenPassword_CheckNotEightCharacters_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
         Assert.assertFalse(checkUserRegistration.checkPassword("asd123"));
     }
 
     @Test
     public void whenGivenPassword_CheckAtleastOneUpperCase_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
-        Assert.assertTrue(checkUserRegistration.checkPassword("abcdE123"));
+        Assert.assertTrue(checkUserRegistration.checkPassword("abcdE$12"));
     }
 
     @Test
     public void whenGivenPassword_CheckWithoutUpperCase_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
         Assert.assertFalse(checkUserRegistration.checkPassword("asdf123"));
     }
 
     @Test
     public void whenGivenPassword_CheckOneNumericNumber_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
-        Assert.assertTrue(checkUserRegistration.checkPassword("asdF1234"));
+        Assert.assertTrue(checkUserRegistration.checkPassword("asdF$124"));
     }
 
     @Test
     public void whenGivenPassword_CheckWithoutNumericNumber_ShouldReturnBoolean() {
-        UserRegistration checkUserRegistration  = new UserRegistration();
-        Assert.assertFalse(checkUserRegistration.checkPassword("asdf"));
+        Assert.assertFalse(checkUserRegistration.checkPassword("asdF"));
     }
+
 }
